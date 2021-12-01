da6d1b401e3779029e0bc7f802a4a0e5177ec94d
Gui-lira <gui.lira.martins@gmail.com>
2021-12-01 18:09:15 -0300

    indexização dos services

---
+++ server/services/index.js
@@ -0,0 +1,11 @@
+const deleteOne = require('./delete');
+const find = require('./find');
+const insert = require('./insert');
+const update = require('./update');
+
+module.exports = {
+    update,
+    insert,
+    find,
+    deleteOne,
+}
